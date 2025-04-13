f8457d4443a1acd3b9e0dca4c74297b6a4e8bb65
Karl George <karlgeorge87@gmail.com>
2025-04-13 15:43:19 +0200

    Fix class definition

--- player.py
+++ player.py
@@ -3,15 +3,13 @@ from constants import PLAYER_RADIUS
 from circleshape import CircleShape
 
 
-import pygame
-from constants import *
-from circleshape import CircleShape
-
-
-def Player(CircleShape):
+class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -20,6 +18,3 @@ def Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-
-    def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 2)
